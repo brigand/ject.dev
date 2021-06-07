131bcccd1806c09094ce2a273e4e46ee49238553
Frankie Bagnardi <f.bagnardi@gmail.com>
2021-06-06 17:28:09 -0700

    feat: impl QuadSplit

--- src/comp/QuadSplit.js
+++ src/comp/QuadSplit.js
@@ -5,48 +5,195 @@ import { Col, Row } from './Flex';
 const splitBasis = (percent) => {
   const SCALE = 10000;
   const p = Math.floor(percent * SCALE);
-  const shared = { flexGrow: 1, flexShrink: 1 };
+  const shared = { flexShrink: 1 };
   return {
-    a: { flexBasis: String(p), ...shared },
-    b: { flexBasis: String(SCALE - p), ...shared },
+    a: { flexGrow: String(p), ...shared },
+    b: { flexGrow: String(SCALE - p), ...shared },
   };
 };
 
 const Cell = styled.section`
-  border: 1px solid transparent;
+  position: relative;
+  border: 1px solid var(--ij-fg);
+`;
 
-  *:nth-of-type(1) > &:nth-of-type(1) {
-    border-right-color: var(--ij-fg);
-    border-bottom-color: var(--ij-fg);
+const DividerX = styled.button`
+  appearance: none;
+  display: block;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: calc(${(props) => props.percent * 100}% - var(--quad-split-divider) / 2);
+  width: var(--quad-split-divider);
+  background: blue;
+
+  &:hover {
+    background: white;
   }
-  *:nth-of-type(2) > &:nth-of-type(1) {
-    border-bottom-color: var(--ij-fg);
+`;
+
+const DividerY = styled.button`
+  appearance: none;
+  display: block;
+  position: absolute;
+  top: calc(${(props) => props.percent * 100}% - var(--quad-split-divider) / 2);
+  width: 100%;
+  left: 0;
+  right: 0;
+  height: var(--quad-split-divider);
+  background: red;
+
+  &:hover {
+    background: white;
   }
-  *:nth-of-type(2) > &:nth-of-type(2) {
-    border-left-color: var(--ij-fg);
+`;
+
+const Net = styled.div`
+  display: block;
+  position: fixed;
+  top: -1000px;
+  right: -1000px;
+  bottom: -1000px;
+  left: -1000px;
+  z-index: 100000;
+  // background: rgba(255, 255, 255, 0.2);
+`;
+
+function Divider({
+  vertical,
+  split,
+  percent,
+  isSizing,
+  onPress,
+  onRelease,
+  onChange,
+}) {
+  const start = React.useRef(null);
+
+  const Tag = vertical ? DividerX : DividerY;
+
+  const getLength = (event) => (vertical ? event.pageX : event.pageY);
+  const getSize = (event) => {
+    const box = event.currentTarget.parentElement.getBoundingClientRect();
+    return vertical ? box.width : box.height;
+  };
+
+  return (
+    <Tag
+      percent={percent}
+      onMouseDown={(event) => {
+        start.current = { length: getLength(event), size: getSize(event), percent };
+        onPress();
+      }}
+      onMouseUp={() => {
+        start.current = null;
+        onRelease();
+      }}
+      onMouseMove={(event) => {
+        if (!start.current || !isSizing) {
+          return;
+        }
+
+        const length = getLength(event);
+        const delta = length - start.current.length;
+        onChange(start.current.percent + delta / start.current.size);
+      }}
+    >
+      {isSizing && <Net />}
+    </Tag>
+  );
+}
+
+function useSplit({ ident, vertical, initial, sizing, setSizing }) {
+  const [percent, setPercent] = React.useState(initial);
+
+  return {
+    value: percent,
+    basis: splitBasis(percent),
+    set: setPercent,
+    sizing,
+    setSizing,
+    divider: (
+      <Divider
+        vertical={ident === 'x'}
+        percent={percent}
+        isSizing={sizing === ident}
+        onPress={() => {
+          setSizing(ident);
+        }}
+        onRelease={() => {
+          setSizing(null);
+        }}
+        onChange={(percent) => {
+          setPercent(percent);
+        }}
+      />
+    ),
+  };
+}
+
+function useSplits() {
+  const [sizing, setSizing] = useState(null);
+
+  const x = useSplit({
+    ident: 'x',
+    vertical: false,
+    initial: 0.5,
+    sizing,
+    setSizing,
+  });
+  const y1 = useSplit({
+    ident: 'y1',
+    vertical: true,
+    initial: 0.5,
+    sizing,
+    setSizing,
+  });
+  const y2 = useSplit({
+    ident: 'y2',
+    vertical: true,
+    initial: 0.5,
+    sizing,
+    setSizing,
+  });
+
+  return { x, y1, y2 };
+}
+
+const Col2 = styled(Col)`
+  position: relative;
+`;
+
+const QuadBox = styled(Row)`
+  height: 100%;
+  position: relative;
+
+  & > button,
+  & > * > button {
+    padding: 0;
+    border: none;
+    --quad-split-divider: 8px;
   }
 `;
 
 function QuadSplit(props) {
-  const [x, setX] = useState(props.initialX || 0.5);
-  const [y1, setY1] = useState(props.initialY1 || 0.5);
-  const [y2, setY2] = useState(props.initialY2 || 0.5);
-
-  const basisRow = splitBasis(x);
-  const basisLeft = splitBasis(y1);
-  const basisRight = splitBasis(y2);
+  const splits = useSplits();
 
   return (
-    <Row horizontal="stretch" style={{ height: '100%' }}>
-      <Col style={basisRow.a}>
-        <Cell style={basisLeft.a}>C1</Cell>
-        <Cell style={basisLeft.b}>C2</Cell>
-      </Col>
-      <Col style={basisRow.b}>
-        <Cell style={basisRight.a}>C3</Cell>
-        <Cell style={basisRight.b}>C4</Cell>
-      </Col>
-    </Row>
+    <QuadBox horizontal="stretch">
+      <Col2 style={splits.x.basis.a}>
+        <Cell style={splits.y1.basis.a}>C1</Cell>
+        <Cell style={splits.y1.basis.b}>C2</Cell>
+        {splits.y1.divider}
+      </Col2>
+      <Col2 style={splits.x.basis.b}>
+        <Cell style={splits.y2.basis.a}>C3</Cell>
+        <Cell style={splits.y2.basis.b}>C4</Cell>
+
+        {splits.y2.divider}
+      </Col2>
+      {splits.x.divider}
+    </QuadBox>
   );
 }
 
