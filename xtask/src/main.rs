aa8e9c264ff49a542e58ffd0a573ccb6544e80c2
Frankie Bagnardi <f.bagnardi@gmail.com>
2021-07-25 15:48:31 -0700

    build: provision command in xtask

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,5 +1,8 @@
 use std::{
-    env, fs,
+    env,
+    ffi::OsString,
+    fs::{self, File},
+    os::unix::prelude::OsStringExt,
     path::{Path, PathBuf},
     process::Command,
 };
@@ -15,8 +18,10 @@ fn main() {
 
 fn try_main() -> Result<(), DynError> {
     let task = env::args().nth(1);
-    match task.as_ref().map(|it| it.as_str()) {
+    let arg1 = env::args().nth(2);
+    match task.as_deref() {
         Some("dist") => dist()?,
+        Some("provision") => provision(arg1.as_deref())?,
         _ => print_help(),
     }
     Ok(())
@@ -25,7 +30,11 @@ fn try_main() -> Result<(), DynError> {
 fn print_help() {
     eprintln!(
         "Tasks:
-dist            builds application and man pages
+dist                       builds application to target/dist/ject-server
+provision [ssh_args]       sets up a server to be able to run ject.dev
+    ssh_args: space-delimited list of initial ssh arguments
+              defaults to \"ject-root\" (without quotes)
+              define a ject-root host in ~/.ssh/config to have the default work
 "
     )
 }
@@ -41,7 +50,41 @@ fn dist() -> Result<(), DynError> {
     Ok(())
 }
 
-static BUILDER_TAG: &str = "ject-musl-builder";
+fn provision(ssh_args: Option<&str>) -> Result<(), DynError> {
+    let ssh_args = ssh_args.unwrap_or("ject-root");
+    let mut cmd = Command::new("ssh");
+    cmd.args(ssh_args.split_whitespace());
+    // cmd.arg("bash");
+    // cmd.arg("-c");
+
+    let write_authorized_key = format!(
+        r#"key="{}"; grep -F "$(printf '%s' "$key" | awk '{{print $2}}')" /home/ject/.ssh/authorized_keys || printf '%s\n' "$key" >> /home/ject/.ssh/authorized_keys"#,
+        get_ssh_pub()?
+    );
+    let bash_commands = vec![
+        "id -u ject >/dev/null 2>&1 || ( echo 'Creating group and user' ; groupadd ject ; useradd -g ject --home-dir /home/ject --shell /bin/bash ject )",
+        "mkdir -p /home/ject/app/letsencrypt/{ssl,nonce}",
+        "mkdir -p /home/ject/.ssh",
+        "touch /home/ject/.ssh/authorized_keys",
+        &write_authorized_key,
+        "chown ject -R /home/ject/{app,.ssh}",
+        "chmod -R 600 /home/ject/app/letsencrypt/",
+        "echo 'End of bash commands!'",
+    ]
+    .join(" && ");
+    cmd.arg(&bash_commands);
+
+    println!("Executing command: {:#?}", cmd);
+
+    let status = cmd.status()?;
+    if !status.success() {
+        Err("The provision ssh command failed")?;
+    }
+
+    Ok(())
+}
+
+// static BUILDER_TAG: &str = "ject-musl-builder";
 // static BUILDER_IMAGE: &str = BUILDER_TAG;
 static BUILDER_IMAGE: &str = "brigand/rust-musl-builder";
 
@@ -170,4 +213,29 @@ fn docker_command() -> Command {
     let mut cmd = Command::new("docker");
     cmd.current_dir(project_root());
     cmd
+}
+
+fn get_home() -> Result<PathBuf, DynError> {
+    let output = Command::new("/bin/bash")
+        .arg("-c")
+        .arg("printf '%s' ~")
+        .output()
+        .map_err(|err| format!("[xtask/get_home] Unable to execute sh, error: {:?}", err))?;
+    if !output.status.success() {
+        eprintln!("[xtask/get_home] output: {:?}", output);
+        Err("[xtask/get_home] shell command unsuccessful")?;
+    }
+    if output.stdout.is_empty() {
+        Err("[xtask/get_home] shell command returned no output")?;
+    }
+    if output.stdout.len() == 1 {
+        Err("[xtask/get_home] shell command returned a single byte (too short)")?;
+    }
+
+    Ok(PathBuf::from(OsString::from_vec(output.stdout)))
+}
+
+fn get_ssh_pub() -> Result<String, DynError> {
+    let key = std::fs::read_to_string(get_home()?.join(".ssh/id_rsa.pub"))?;
+    Ok(key.trim().split('\n').next().unwrap().trim().to_string())
 }
