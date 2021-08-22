5674a97831279e638662d2072548da5e3e475bf8
Frankie Bagnardi <f.bagnardi@gmail.com>
2021-08-22 14:33:23 -0700

    feat: UrlWrapper#state and document all methods

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -4,6 +4,7 @@ export class UrlWrapper {
   #location;
   #history;
   #params;
+
   constructor(location, history, searchParams = null) {
     this.#location = location;
     this.#history = history;
@@ -11,21 +12,50 @@ export class UrlWrapper {
       searchParams ?? new URLSearchParams(location.search?.replace('?', '') ?? '');
   }
 
+  /**
+   * With no arguments, returns the URLSearchParams instance.
+   * Pass a key to get a specific query param value string.
+   */
   query(key = null) {
     return key == null ? this.#params : this.#params.get(key);
   }
 
+  /**
+   * With no arguments, returns the state object.
+   * Pass a key to get a specific state field value.
+   */
+  state(key = null) {
+    return key == null ? this.#location.state : this.#location.state?.[key];
+  }
+
+  /**
+   * Replaces the pathname portion of the location object.
+   * Returns an updated copy.
+   */
   withPath(pathname) {
     const location = { ...this.#location, pathname };
     return new UrlWrapper(location, this.#history);
   }
 
+  /**
+   * Set or delete (pass null for value) a specific query param.
+   * Returns an updated copy.
+   */
   withQuery(k, v) {
     const params = new URLSearchParams(this.#params);
-    params.set(k, v);
+    if (v == null) {
+      params.delete(k);
+    } else {
+      params.set(k, v);
+    }
+
     return new UrlWrapper(this.#location, this.#history, params);
   }
 
+  /**
+   * Resets state to an empty object.
+   * Returns an updated copy.
+   */
   withEmptyState() {
     const location = {
       ...this.#location,
@@ -34,6 +64,10 @@ export class UrlWrapper {
     return new UrlWrapper(location, this.#history, this.#params);
   }
 
+  /**
+   * Set a specific state field.
+   * Returns an updated copy.
+   */
   withState(k, v) {
     const location = {
       ...this.#location,
@@ -42,22 +76,36 @@ export class UrlWrapper {
     return new UrlWrapper(location, this.#history, this.#params);
   }
 
+  /**
+   * Builds a react-router location object.
+   */
   buildLocation() {
     const search = this.#params.toString();
     return { ...this.#location, search: '?' + search };
   }
 
+  /**
+   * Pushes the location onto the react-router history. Typical end of a chain.
+   */
   applyByPush() {
     this.#history.push(this.buildLocation());
     return this;
   }
 
+  /**
+   * Replace the top location in the react-router history. Typical end of a chain.
+   */
   applyByReplace() {
     this.#history.replace(this.buildLocation());
     return this;
   }
 }
 
+/**
+ * Converts the react-router location to a fluent interface for inspecting/modifying the path/query/state.
+ *
+ * @returns UrlWrapper
+ */
 function useUrl() {
   const location = useLocation();
   const history = useHistory();
